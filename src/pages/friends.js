65459f7274573f9e34a7609fbd8661fcb5e5057a
Yaossg <yaossg@outlook.com>
2023-07-30 16:53:06 +0800

    update friends

--- src/pages/friends.js
+++ src/pages/friends.js
@@ -10,204 +10,176 @@ const FriendList = [
     {
         "avatar": "https://avatars.githubusercontent.com/u/42486439",
         "link": "https://blog.stapxs.cn",
-        "name": "Stapxs",
-        "desc": "林槐"
+        "name": "Stapxs"
     },
     {
         "avatar": "http://q1.qlogo.cn/g?b=qq&nk=2734825077&s=640",
         "link": "https://switefaster.github.io/",
-        "name": "Sebrarin",
-        "desc": "Switefaster"
+        "name": "Sebrarin"
     },
     {
         "avatar": "https://gravatar.loli.net/avatar/d9464e8ea970cefdfb729b7301d04232?s=100&d=mp",
         "link": "https://blog.zapic.moe/",
-        "name": "Zapic",
-        "desc": "榨批客"
+        "name": "Zapic"
     },
     {
         "avatar": "http://q1.qlogo.cn/g?b=qq&nk=754503921&s=640",
         "link": "https://izzel.io/",
-        "name": "IzzelAliz",
-        "desc": "海螺"
+        "name": "IzzelAliz"
     },
     {
         "avatar": "https://avatars.githubusercontent.com/u/8004211",
         "link": "https://blog.ustc-zzzz.net/",
-        "name": "ustc-zzzz",
-        "desc": "土球"
+        "name": "ustc-zzzz"
     },
     {
         "avatar": "https://assets.doodlehuang.com/myface.png",
         "link": "https://doodlehuang.com/",
-        "name": "Doodle",
-        "desc": "DoodleHuang"
-    },
-    {
-        "avatar": "https://avatars.githubusercontent.com/u/64351788",
-        "link": "https://fullstack-sake.github.io/",
-        "name": "sake",
-        "desc": "冉冉"
-    },
-    {
-        "avatar": "https://4ever-xxxl.github.io/img/avatar.jpg",
-        "link": "https://4ever-xxxl.github.io/",
-        "name": "xlykle",
-        "desc": "磊子哥"
-    },
-    {
-        "avatar": "https://avatars.githubusercontent.com/u/36410093",
-        "link": "https://xukafy.github.io/",
-        "name": "XuKaFy",
-        "desc": "X. K. F."
-    },
-    {
-        "avatar": "http://q1.qlogo.cn/g?b=qq&nk=2921349622&s=640",
-        "link": "https://www.cnblogs.com/timlzh",
-        "name": "Timlzh",
-        "desc": "¯\\_(ツ)_/¯"
-    },
-    {
-        "avatar": "https://malossov.github.io/uploads/avator.png",
-        "link": "https://malossov.github.io/",
-        "name": "Malossov",
-        "desc": "秧黍藜"
-    },
-    {
-        "avatar": "https://avatars.githubusercontent.com/u/49082837?v=4",
-        "link": "https://zbwer.github.io/",
-        "name": "zbwer",
-        "desc": "FsAnna"
-    },
-    {
-        "avatar": "https://avatars.githubusercontent.com/u/108183563",
-        "link": "https://zzzremake.github.io/",
-        "name": "remake",
-        "desc": "ZzzRemake"
-    },
-    {
-        "avatar": "https://avatars.githubusercontent.com/u/61999173",
-        "link": "https://blog.skyecho.fun/",
-        "name": "syrinka",
-        "desc": "上行线路"
-    },
-    {
-        "avatar": "http://q1.qlogo.cn/g?b=qq&nk=463135821&s=640",
-        "link": "https://codeforces.com/profile/lotato",
-        "name": "lotato",
-        "desc": "土豆"
-    },
-    {
-        "avatar": "https://avatars.githubusercontent.com/u/69978374?v=4",
-        "link": "https://github.com/RisingEntropy",
-        "name": "Entropy",
-        "desc": "RisingEntropy"
-    },
-    {
-        "avatar": "https://fattyray.github.io/fattyrays_homepage/profile.jpg",
-        "link": "https://fattyray.github.io/fattyrays_homepage/",
-        "name": "Sary",
-        "desc": "fattyrays"
-    },
-    {
-        "avatar": "https://mrcai.dev/_astro/avatar.a5e28833.webp",
-        "link": "https://mrcai.dev/",
-        "name": "mrcai",
-        "desc": "mrcaidev"
-    },
-    {
-        "avatar": "http://q1.qlogo.cn/g?b=qq&nk=2624646185&s=640",
-        "link": "https://focess.top/",
-        "name": "sumijie",
-        "desc": "MidCoard"
-    },
-    {
-        "avatar": "https://avatars.githubusercontent.com/u/101344459?v=4",
-        "link": "https://songyu318.github.io/",
-        "name": "SoCoco",
-        "desc": "羽姐姐"
-    },
-    {
-        "avatar": "https://edwardssss.github.io/user/show_myself.png",
-        "link": "https://edwardssss.github.io/",
-        "name": "Edwardssss",
-        "desc": "熊爷"
-    },
-    {
-        "avatar": "http://q1.qlogo.cn/g?b=qq&nk=3568921382&s=640",
-        "link": "https://www.cnblogs.com/IrisHyaline",
-        "name": "IrisHyaline",
-        "desc": "透明桥"
-    },
-    {
-        "avatar": "http://q1.qlogo.cn/g?b=qq&nk=2083953860&s=640",
-        "link": "https://blog.csdn.net/weixin_44111457",
-        "name": "lsr",
-        "desc": "融融"
+        "name": "Doodle H."
     },
     {
         "avatar": "https://avatars.githubusercontent.com/u/32266528?v=4",
         "link": "https://github.com/Zhengfourth",
-        "name": "Zhengfourth",
-        "desc": "国集爷"
-    },
-    {
-        "avatar": "https://avatars.githubusercontent.com/u/32478104",
-        "link": "https://github.com/KunoSayo",
-        "name": "KunoSayo",
-        "desc": "ljyys"
-    },
-    {
-        "avatar": "http://www.sh1no.icu/avr.png",
-        "link": "http://www.sh1no.icu/",
-        "name": "Shino",
-        "desc": "诗诺"
-    },
-    {
-        "avatar": "https://avatars.githubusercontent.com/u/102424651?v=4",
-        "link": "https://je3ter.github.io/",
-        "name": "Je3ter",
-        "desc": "安娜苏"
+        "name": "Zhengfourth"
     },
     {
         "avatar": "https://avatars.githubusercontent.com/u/25865657?v=4",
         "link": "https://my.minecraft.kim/",
-        "name": "HikariLan",
-        "desc": "贺兰星辰"
+        "name": "HikariLan"
+    },
+    {
+        "avatar": "https://avatars.githubusercontent.com/u/32478104",
+        "link": "https://github.com/KunoSayo",
+        "name": "KunoSayo"
+    },
+    {
+        "avatar": "https://avatars.githubusercontent.com/u/64351788",
+        "link": "https://fullstack-sake.github.io/",
+        "name": "sake"
+    },
+    {
+        "avatar": "https://4ever-xxxl.github.io/img/avatar.jpg",
+        "link": "https://4ever-xxxl.github.io/",
+        "name": "xlykle"
+    },
+    {
+        "avatar": "https://avatars.githubusercontent.com/u/36410093",
+        "link": "https://xukafy.github.io/",
+        "name": "XuKaFy"
+    },
+    {
+        "avatar": "http://q1.qlogo.cn/g?b=qq&nk=2921349622&s=640",
+        "link": "https://www.cnblogs.com/timlzh",
+        "name": "Timlzh"
+    },
+    {
+        "avatar": "https://malossov.github.io/uploads/avator.png",
+        "link": "https://malossov.github.io/",
+        "name": "Malossov"
+    },
+    {
+        "avatar": "https://avatars.githubusercontent.com/u/49082837?v=4",
+        "link": "https://zbwer.github.io/",
+        "name": "zbwer"
+    },
+    {
+        "avatar": "https://avatars.githubusercontent.com/u/108183563",
+        "link": "https://zzzremake.github.io/",
+        "name": "ZzzRemake"
+    },
+    {
+        "avatar": "https://avatars.githubusercontent.com/u/61999173",
+        "link": "https://blog.skyecho.fun/",
+        "name": "syrinka"
+    },
+    {
+        "avatar": "http://q1.qlogo.cn/g?b=qq&nk=463135821&s=640",
+        "link": "https://codeforces.com/profile/lotato",
+        "name": "lotato"
+    },
+    {
+        "avatar": "https://avatars.githubusercontent.com/u/69978374?v=4",
+        "link": "https://github.com/RisingEntropy",
+        "name": "R. Entropy"
+    },
+    {
+        "avatar": "https://fattyray.github.io/fattyrays_homepage/profile.jpg",
+        "link": "https://fattyray.github.io/fattyrays_homepage/",
+        "name": "fattyrays"
+    },
+    {
+        "avatar": "https://mrcai.dev/_astro/avatar.a5e28833.webp",
+        "link": "https://mrcai.dev/",
+        "name": "mrcaidev"
+    },
+    {
+        "avatar": "http://q1.qlogo.cn/g?b=qq&nk=2624646185&s=640",
+        "link": "https://focess.top/",
+        "name": "sumijie"
+    },
+    {
+        "avatar": "https://avatars.githubusercontent.com/u/101344459?v=4",
+        "link": "https://songyu318.github.io/",
+        "name": "SoCoco"
+    },
+    {
+        "avatar": "https://edwardssss.github.io/user/show_myself.png",
+        "link": "https://edwardssss.github.io/",
+        "name": "Edwardssss"
+    },
+    {
+        "avatar": "http://q1.qlogo.cn/g?b=qq&nk=3568921382&s=640",
+        "link": "https://www.cnblogs.com/IrisHyaline",
+        "name": "IrisHyaline"
+    },
+    {
+        "avatar": "http://q1.qlogo.cn/g?b=qq&nk=2083953860&s=640",
+        "link": "https://blog.csdn.net/weixin_44111457",
+        "name": "lsr"
+    },
+    {
+        "avatar": "http://www.sh1no.icu/avr.png",
+        "link": "http://www.sh1no.icu/",
+        "name": "Shino"
+    },
+    {
+        "avatar": "https://avatars.githubusercontent.com/u/102424651?v=4",
+        "link": "https://je3ter.github.io/",
+        "name": "Je3ter"
     },
     {
         "avatar": "http://q1.qlogo.cn/g?b=qq&nk=185248912&s=640",
         "link": "https://www.riojasonc.com/",
-        "name": "RioJasonC",
-        "desc": ""
+        "name": "RioJasonC"
     },
     {
         "avatar": "https://avatars.githubusercontent.com/u/108604218?v=4",
         "link": "https://touhouxingchen.github.io/",
-        "name": "XCCY",
-        "desc": "星辰春雨"
+        "name": "XCCY"
     },
     {
         "avatar": "https://lostacnet.top/images/avatar.jpg",
         "link": "https://lostacnet.top/",
-        "name": "LO_StacNet",
-        "desc": ""
+        "name": "LO_StacNet"
     },
     {
         "avatar": "https://avatars.githubusercontent.com/u/86053421?v=4",
         "link": "https://ericzhang1412.github.io/",
-        "name": "EricZhang",
-        "desc": ""
+        "name": "EricZhang"
     },
     {
         "avatar": "https://touhouxingchen.github.io/image/xfzn.png",
         "link": "https://dummywj.github.io/",
-        "name": "Cherry_ywj",
-        "desc": ""
+        "name": "Cherry_ywj"
+    }, 
+    {
+        "avatar": "https://image.foxsuzuran.top/images/2023/04/14/QQ20230108024840.jpg",
+        "link": "https://blog.foxsuzuran.top/",
+        "name": "FoxSuzuran"
     }
 ]
 
-function FriendCard({avatar, link, name, desc}) {
+function FriendCard({avatar, link, name}) {
     return (
         <div className={clsx('col col--4', styles.cell)}>
             <div className={styles.card}>
@@ -216,8 +188,6 @@ function FriendCard({avatar, link, name, desc}) {
                         <img src={avatar} height="100" width="100"/>
                         <div>
                             <h1>{name}</h1>
-                            <em></em>
-                            <p>{desc}</p>
                         </div>
                     </div>
                 </a>
